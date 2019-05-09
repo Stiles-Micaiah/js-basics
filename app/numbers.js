f3fba2d939ff408294511b0e264d2b399d5cd139
Judah <syluxprime40@gmail.com>
2019-05-08 21:11:38 -0600

    gitty titty

---
+++ app/numbers.js
@@ -0,0 +1,29 @@
+// Write a function that uses the addition operator to add two numbers together
+function add(x, y) {
+  return x + y
+}
+
+// Write a function that returns the Cubed value of x
+function cubed(x) {
+  return x^3
+}
+
+// Write a function that will divide two numbers, and will format the returned value
+// as a whole number. (No Decimals)
+function divideWholey(x, y) {
+  x/y
+}
+
+// Write a function that takes in a number and determines if it is even. 
+// It should return a boolean value - true if it is even, false if it is odd.
+function isEven(num) {
+  num % 2 == 0 ? true: false
+}
+
+
+// Given 2 numbers, generate a random number within that range. 
+// Make it inclusive so that the starting and ending numbers are part 
+// of the range.HINT ** Utilize Javascript's Math Class (To the Google!) wtf
+function numberGenerator(num1, num2) {
+  (Math.random() * ((num2 - num1) + 1)) + num1
+}
